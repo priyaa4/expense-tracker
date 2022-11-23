e94ee5be8467ec815e61da1c9a7efad6339fa05e
Priya <priyakharel83@gmail.com>
2022-11-23 18:00:33 +0545

    onboarding half

---
+++ lib/core/presentation/onboarding.dart
@@ -0,0 +1,88 @@
+import 'package:expense_bud/config/theme.dart';
+import 'package:expense_bud/core/presentation/choose_currency.dart';
+import 'package:expense_bud/core/util/extensions.dart';
+
+import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+
+import '../../config/constant.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(Insets.lg),
+        child: Column(
+          children: [
+            const Gap(64),
+            Image.asset(AppImages.logo, height: 24),
+            const Gap(Insets.lg),
+            const Info(
+              'Add entries',
+              'Keep track of your income and expenses',
+              PhosphorIcons.tray,
+            ),
+            const Gap(Insets.lg),
+            const Info(
+              'Check insights',
+              'Detailed weekly and monthly charts based on your entries',
+              PhosphorIcons.lightning,
+            ),
+            const Gap(Insets.lg),
+            const Info(
+              'Make right decisions',
+              'Control your money flow and stay on top of your game',
+              PhosphorIcons.medal,
+            ),
+            const Spacer(),
+            Button(
+              "GET STARTED",
+              onTap: () => context.push(const ChooseCurrencyPage()),
+            ),
+            const Gap(Insets.lg),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Info extends StatelessWidget {
+  final IconData icon;
+  final String title, subtitle;
+  const Info(this.title, this.subtitle, this.icon, {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          icon,
+          size: IconSizes.lg,
+        ),
+        Gap.md,
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: context.textTheme.subtitle1,
+              ),
+              Gap.sm,
+              Text(
+                subtitle,
+                style: context.textTheme.subtitle1!
+                    .copyWith(color: AppColors.kGrey),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
