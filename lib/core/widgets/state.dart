80a3a9d1f8460edb45f2bd6bc7eff1ac11e9c4f7
Priya <priyakharel83@gmail.com>
2022-11-26 18:15:51 +0545

    widgets state done

---
+++ lib/core/widgets/state.dart
@@ -0,0 +1,50 @@
+import 'package:expense_bud/config/theme.dart';
+import 'package:expense_bud/core/widgets/gap.dart';
+import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+
+import '../../config/constant.dart';
+
+enum Variant { noData, error }
+
+class NoDataOrError extends StatelessWidget {
+  final String msg;
+  final Variant variant;
+
+  const NoDataOrError(this.msg, {this.variant = Variant.noData, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final color =
+        variant == Variant.noData ? AppColors.kGrey : AppColors.kError;
+    return Padding(
+      padding: const EdgeInsets.all(Insets.lg),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 64,
+            width: 64,
+            decoration: BoxDecoration(
+              borderRadius: Corners.mdBorder,
+              color: color.withOpacity(0.2),
+            ),
+            child: Icon(
+              variant == Variant.noData
+                  ? PhosphorIcons.rocketFill
+                  : PhosphorIcons.finnTheHumanFill,
+              color: color,
+              size: IconSizes.lg,
+            ),
+          ),
+          Gap.md,
+          Text(
+            msg,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
