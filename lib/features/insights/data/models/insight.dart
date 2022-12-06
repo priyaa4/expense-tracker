80d7bd88483aa98af48fb4846a7a4d5e1a8dcf8f
Priya <priyakharel83@gmail.com>
2022-12-06 19:03:04 +0545

    datasource

--- lib/features/insights/data/models/insight.dart
+++ lib/features/insights/data/models/insight.dart
@@ -1,15 +1,12 @@
-// import 'package:expense_bud/core/data/mappers/expense.dart';
+import '../../../../core/data/models/expence.dart';
+import '../../domain/entities/insight.dart';
 
-// import '../../../../core/data/models/expence.dart';
-
-
-// class InsightModel extends InsightEntity {
-//   InsightModel(
-//     List<ExpenseModel> cur,
-//     List<ExpenseModel> prev,
-//   ) : super(
-//           cur.map((e) => e.toEntity()).toList(),
-//           prev.map((e) => e.toEntity()).toList(),
-//         );
-
-// }
+class InsightModel extends InsightEntity {
+  InsightModel(
+    List<ExpenseModel> cur,
+    List<ExpenseModel> prev,
+  ) : super(
+          cur.map((e) => e.toEntity()).toList(),
+          prev.map((e) => e.toEntity()).toList(),
+        );
+}
