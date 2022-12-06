79590ce9093e07d05defff0c1d8593b62936d221
Priya <priyakharel83@gmail.com>
2022-12-06 19:03:42 +0545

    insight repo

---
+++ lib/features/insights/data/repositories/insight_repository_impl.dart
@@ -0,0 +1,26 @@
+import 'package:expense_bud/features/insights/domain/entities/insight_period.dart';
+import 'package:expense_bud/features/insights/domain/entities/insight.dart';
+import 'package:expense_bud/core/failure/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:expense_bud/features/insights/domain/repositories/insight_repository.dart';
+
+import '../datasource/local_datasource.dart';
+
+class InsightRepository implements IInsightRepository {
+  final IInsightLocalDataSource _insightLocalDataSource;
+  InsightRepository(this._insightLocalDataSource);
+  @override
+  Future<Either<Failure, InsightEntity>> getInsightsForPeriod(
+    InsightPeriod period,
+  ) async {
+    try {
+      final insight =
+          await _insightLocalDataSource.getInsightsForPeriod(period);
+      return Right(
+        InsightEntity(insight.cur, insight.prev),
+      );
+    } catch (e) {
+      return Left(CacheGetFailure());
+    }
+  }
+}
