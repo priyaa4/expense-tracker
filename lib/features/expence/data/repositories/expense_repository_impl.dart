e29126363fa6cce6bf4b3334ef295b9c7623dc0a
Priya <priyakharel83@gmail.com>
2022-12-07 19:27:01 +0545

    domain editing of features

--- lib/features/expence/data/repositories/expense_repository_impl.dart
+++ lib/features/expence/data/repositories/expense_repository_impl.dart
@@ -1,51 +1,48 @@
-// import 'package:expense_bud/core/data/mappers/expense.dart';
+import 'package:expense_bud/core/domain/entities/expense.dart';
+import 'package:expense_bud/core/failure/failure.dart';
+import 'package:dartz/dartz.dart';
 
-// import 'package:expense_bud/core/domain/entities/expense.dart';
-// import 'package:expense_bud/core/failure/failure.dart';
-// import 'package:dartz/dartz.dart';
-// import 'package:expense_bud/features/expense/domain/repositories/expense_repository.dart';
+class ExpenseRepository implements IExpenseRepository {
+  final IExpenseLocalDataSource _localDataSource;
+  ExpenseRepository(this._localDataSource);
 
-// class ExpenseRepository implements IExpenseRepository {
-//   final IExpenseLocalDataSource _localDataSource;
-//   ExpenseRepository(this._localDataSource);
+  @override
+  Future<Either<Failure, Unit>> createExpenseEntry(
+      ExpenseEntity expense) async {
+    try {
+      await _localDataSource.createExpenseEntry(expense.toModel());
+      return const Right(unit);
+    } catch (e) {
+      return Left(CachePutFailure());
+    }
+  }
 
-//   @override
-//   Future<Either<Failure, Unit>> createExpenseEntry(
-//       ExpenseEntity expense) async {
-//     try {
-//       await _localDataSource.createExpenseEntry(expense.toModel());
-//       return const Right(unit);
-//     } catch (e) {
-//       return Left(CachePutFailure());
-//     }
-//   }
+  @override
+  Stream<Either<Failure, Map<String, List<ExpenseEntity>>>>
+      watchMonthlyEntries() async* {
+    try {
+      yield* _localDataSource.watchMonthlyEntries().map(
+            (event) => Right(
+              event.map(
+                (key, value) => MapEntry(
+                  key,
+                  value.map((e) => e.toEntity()).toList(),
+                ),
+              ),
+            ),
+          );
+    } catch (e) {
+      yield Left(CacheGetFailure());
+    }
+  }
 
-//   @override
-//   Stream<Either<Failure, Map<String, List<ExpenseEntity>>>>
-//       watchMonthlyEntries() async* {
-//     try {
-//       yield* _localDataSource.watchMonthlyEntries().map(
-//             (event) => Right(
-//               event.map(
-//                 (key, value) => MapEntry(
-//                   key,
-//                   value.map((e) => e.toEntity()).toList(),
-//                 ),
-//               ),
-//             ),
-//           );
-//     } catch (e) {
-//       yield Left(CacheGetFailure());
-//     }
-//   }
-
-//   @override
-//   Future<Either<Failure, Unit>> eraseEntries() async {
-//     try {
-//       await _localDataSource.eraseEntries();
-//       return const Right(unit);
-//     } catch (e) {
-//       return Left(CacheGetFailure());
-//     }
-//   }
-// }
+  @override
+  Future<Either<Failure, Unit>> eraseEntries() async {
+    try {
+      await _localDataSource.eraseEntries();
+      return const Right(unit);
+    } catch (e) {
+      return Left(CacheGetFailure());
+    }
+  }
+}
---
+++ lib/features/insights/presentation/expense_tab.dart
@@ -0,0 +1,216 @@
+// import 'package:expense_bud/config/theme.dart';
+// import 'package:expense_bud/core/util/extensions.dart';
+
+// import 'package:expense_bud/core/widgets/gap.dart';
+// import 'package:expense_bud/core/widgets/state.dart';
+// import 'package:expense_bud/core/domain/entities/expense.dart';
+
+// import 'package:expense_bud/features/settings/presentation/providers/settings_provider.dart';
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+
+// import '../../../config/constant.dart';
+
+// enum Tab { today, month }
+
+// class ExpenseTab extends StatefulWidget {
+//   const ExpenseTab({Key? key}) : super(key: key);
+
+//   @override
+//   State<ExpenseTab> createState() => _ExpenseTabState();
+// }
+
+// class _ExpenseTabState extends State<ExpenseTab> {
+//   Tab currentTab = Tab.today;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         Padding(
+//           padding: const EdgeInsets.symmetric(
+//             horizontal: Insets.lg,
+//             vertical: Insets.md,
+//           ),
+//           child: Container(
+//             height: 40,
+//             clipBehavior: Clip.hardEdge,
+//             decoration: BoxDecoration(
+//               color: AppColors.kGrey200,
+//               borderRadius: Corners.lgBorder,
+//             ),
+//             child: Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: Tab.values
+//                   .map(
+//                     (e) => Expanded(
+//                       child: TabItem((e.name.titleCaseSingle()),
+//                           value: e,
+//                           groupValue: currentTab,
+//                           onChanged: (value) =>
+//                               setState(() => currentTab = value)),
+//                     ),
+//                   )
+//                   .toList(),
+//             ),
+//           ),
+//         ),
+//         IndexedStack(
+//           index: currentTab.index,
+//           children: const [TodayTab(), MonthTab()],
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+// class TodayTab extends StatelessWidget {
+//   const TodayTab({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final expenseProvider = context.watch<ExpenseProvider>();
+//     final entries = context.watch<ExpenseProvider>().entries;
+//     final date = expenseProvider.currentDateString;
+//     final key = expenseProvider.currentDateKey;
+
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(horizontal: Insets.lg),
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.stretch,
+//         children: [
+//           ExpenseHeader(date: date),
+//           entries.when(
+//             loading: (msg) => const Center(child: Text("Loading...")),
+//             done: (entries) => entries[key] == null
+//                 ? const NoDataOrError("No entries today")
+//                 : ExpenseList(entries[key]!),
+//             error: (msg) => NoDataOrError(msg!, variant: Variant.error),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class MonthTab extends StatelessWidget {
+//   const MonthTab({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final expenseProvider = context.watch<ExpenseProvider>();
+//     final money = context.watch<SettingsProvider>().money;
+//     final entries = expenseProvider.entries;
+
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(horizontal: Insets.lg),
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.stretch,
+//         children: [
+//           entries.when(
+//             loading: (msg) => const Center(child: Text("Loading...")),
+//             done: (data) => data.isEmpty
+//                 ? const NoDataOrError("No entries this month")
+//                 : Column(
+//                     children: [
+//                       ...data.keys
+//                           .map(
+//                             (key) => Column(
+//                               children: [
+//                                 ExpenseHeader(
+//                                   date: expenseProvider
+//                                       .getReadableDateString(key),
+//                                   amount: money.formatValue(
+//                                     expenseProvider.getDailyTotal(data[key]),
+//                                   ),
+//                                 ),
+//                                 ExpenseList(data[key]!),
+//                                 Gap.md,
+//                               ],
+//                             ),
+//                           )
+//                           .toList()
+//                     ],
+//                   ),
+//             error: (msg) => NoDataOrError(msg!, variant: Variant.error),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class ExpenseList extends StatelessWidget {
+//   final List<ExpenseEntity> entries;
+//   const ExpenseList(this.entries, {Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView.builder(
+//       padding: const EdgeInsets.symmetric(vertical: Insets.sm),
+//       physics: const NeverScrollableScrollPhysics(),
+//       shrinkWrap: true,
+//       itemCount: entries.length,
+//       itemBuilder: (context, i) => ExpenseListItem(entries[i]),
+//     );
+//   }
+// }
+
+// class ExpenseHeader extends StatelessWidget {
+//   final String date;
+//   final String? amount;
+//   const ExpenseHeader({required this.date, this.amount, Key? key})
+//       : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//       children: [
+//         Text(date, style: context.textTheme.bodyText1!),
+//         Text(
+//           amount ?? "",
+//           style: context.textTheme.bodyText2!.copyWith(
+//             color: const Color(0xFFE58D67),
+//             fontSize: FontSizes.s16,
+//           ),
+//         )
+//       ],
+//     );
+//   }
+// }
+
+// class TabItem extends StatelessWidget {
+//   final String title;
+//   final Tab value;
+//   final Tab groupValue;
+//   final ValueChanged<Tab> onChanged;
+
+//   const TabItem(
+//     this.title, {
+//     required this.value,
+//     required this.groupValue,
+//     required this.onChanged,
+//     Key? key,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final isSelected = value == groupValue;
+//     return AnimatedContainer(
+//       duration: const Duration(milliseconds: 150),
+//       decoration: BoxDecoration(
+//         color: isSelected ? AppColors.kDark : AppColors.kGrey200,
+//         borderRadius: Corners.lgBorder,
+//       ),
+//       child: Center(
+//         child: Text(
+//           title,
+//           style: context.textTheme.bodyText1!.copyWith(
+//             color: isSelected ? Colors.white : AppColors.kDark,
+//           ),
+//         ),
+//       ),
+//     ).onTap(() => onChanged(value));
+//   }
+// }
