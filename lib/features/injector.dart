ffa7c8b84bfd57488416602c5b9935a5333a12d5
Priya <priyakharel83@gmail.com>
2022-12-22 20:15:48 +0545

    final test going on

--- lib/features/injector.dart
+++ lib/features/injector.dart
@@ -1,83 +1,84 @@
-// import 'package:expense_bud/core/data/models/expense.dart';
-// import 'package:expense_bud/core/utils/device.dart';
-// import 'package:expense_bud/features/expense/data/datasources/local_datasource.dart';
-// import 'package:expense_bud/features/expense/data/repositories/expense_repository_impl.dart';
-// import 'package:expense_bud/features/expense/domain/repositories/expense_repository.dart';
-// import 'package:expense_bud/features/expense/domain/usecases/create_entry_usecase.dart';
-// import 'package:expense_bud/features/expense/domain/usecases/erase_entries_usecase.dart';
-// import 'package:expense_bud/features/expense/domain/usecases/get_expenses_usecase.dart';
-// import 'package:expense_bud/features/expense/presentation/provider/expense_provider.dart';
-// import 'package:expense_bud/features/insights/data/datasources/local_datasource.dart';
-// import 'package:expense_bud/features/insights/data/repositories/insight_repository_impl.dart';
-// import 'package:expense_bud/features/insights/domain/repositories/insight_repository.dart';
-// import 'package:expense_bud/features/insights/domain/usecases/get_insights_usecase.dart';
-// import 'package:expense_bud/features/insights/presentation/provider/insights_provider.dart';
-// import 'package:expense_bud/features/settings/data/datasources/user_preference_local_datasource.dart';
-// import 'package:expense_bud/features/settings/data/models/currency.dart';
-// import 'package:expense_bud/features/settings/data/models/user_preference.dart';
-// import 'package:expense_bud/features/settings/data/user_preference_repository_impl.dart';
-// import 'package:expense_bud/features/settings/domain/repositories/user_preference_repository.dart';
-// import 'package:expense_bud/features/settings/domain/usecases/get_user_preference_usecase.dart';
-// import 'package:expense_bud/features/settings/domain/usecases/update_user_preference_usecase.dart';
-// import 'package:expense_bud/features/settings/presentation/providers/settings_provider.dart';
-// import 'package:get_it/get_it.dart';
-// import 'package:hive/hive.dart';
+import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
 
-// final getIt = GetIt.I;
+import '../core/data/models/expense.dart';
+import '../core/util/device.dart';
+import 'expence/data/datasource/local_datasource.dart';
+import 'expence/data/repositories/expense_repository_impl.dart';
+import 'expence/domain/repositories/expense_repository.dart';
+import 'expence/domain/usercases/create_entry_usecase.dart';
+import 'expence/domain/usercases/erase_entries_usecase.dart';
+import 'expence/domain/usercases/get_expenses_usecase.dart';
+import 'expence/presentation/provider/expense_provider.dart';
+import 'insights/data/datasource/local_datasource.dart';
+import 'insights/data/repositories/insight_repository_impl.dart';
+import 'insights/domain/repositories/insight_repository.dart';
+import 'insights/domain/usercases/get_insights_usecase.dart';
+import 'insights/presentation/provider/insights_provider.dart';
+import 'settings/data/datasources/user_preference_local_datasource.dart';
+import 'settings/data/models/currency.dart';
+import 'settings/data/models/user_preference.dart';
+import 'settings/data/user_preference_repository_impl.dart';
+import 'settings/domain/repositories/user_preference_repository.dart';
+import 'settings/domain/usercases/get_user_preference_usecase.dart';
+import 'settings/domain/usercases/update_user_preference_usecase.dart';
+import 'settings/presentation/providers/settings_provider.dart';
 
-// Future<void> initApp() async {
-//   final storage = await getStorageDirectory();
-//   Hive.init(storage);
-//   Hive.registerAdapter<ExpenseModel>(ExpenseModelAdapter());
-//   Hive.registerAdapter<UserPreferenceModel>(UserPreferenceModelAdapter());
-//   Hive.registerAdapter<CurrencyModel>(CurrencyModelAdapter());
+final getIt = GetIt.I;
 
-//   final _expenseDb = await Hive.openBox("expenses.db");
-//   final _preferenceDb = await Hive.openBox("preferences.db");
+Future<void> initApp() async {
+  final storage = await getStorageDirectory();
+  Hive.init(storage);
+  Hive.registerAdapter<ExpenseModel>(ExpenseModelAdapter());
+  Hive.registerAdapter<UserPreferenceModel>(UserPreferenceModelAdapter());
+  Hive.registerAdapter<CurrencyModel>(CurrencyModelAdapter());
 
-//   /// datasources
-//   getIt.registerSingleton<IExpenseLocalDataSource>(
-//       ExpenseLocalDataSource(_expenseDb));
-//   getIt.registerSingleton<IInsightLocalDataSource>(
-//       InsightLocalDataSource(_expenseDb));
-//   getIt.registerSingleton<IUserPreferenceLocalDataSource>(
-//       UserPreferenceLocalDataSource(_preferenceDb));
+  final _expenseDb = await Hive.openBox("expenses.db");
+  final _preferenceDb = await Hive.openBox("preferences.db");
 
-//   /// repositories
-//   getIt.registerSingleton<IExpenseRepository>(ExpenseRepository(getIt()));
-//   getIt.registerSingleton<IInsightRepository>(InsightRepository(getIt()));
-//   getIt.registerSingleton<IUserPreferenceRepository>(
-//       UserPreferenceRepository(getIt()));
+  /// datasources
+  getIt.registerSingleton<IExpenseLocalDataSource>(
+      ExpenseLocalDataSource(_expenseDb));
+  getIt.registerSingleton<IInsightLocalDataSource>(
+      InsightLocalDataSource(_expenseDb));
+  getIt.registerSingleton<IUserPreferenceLocalDataSource>(
+      UserPreferenceLocalDataSource(_preferenceDb));
 
-//   /// usecases
-//   getIt.registerSingleton<GetExpensesUsecase>(GetExpensesUsecase(getIt()));
-//   getIt.registerSingleton<CreateExpenseEntryUsecase>(
-//       CreateExpenseEntryUsecase(getIt()));
-//   getIt.registerSingleton<EraseEntriesUsecase>(EraseEntriesUsecase(getIt()));
-//   getIt.registerSingleton<GetUserPreferenceUsecase>(
-//       GetUserPreferenceUsecase(getIt()));
-//   getIt.registerSingleton<UpdateUserPreferenceUsecase>(
-//       UpdateUserPreferenceUsecase(getIt()));
-//   getIt.registerSingleton<GetInsightsUsecase>(GetInsightsUsecase(getIt()));
+  /// repositories
+  getIt.registerSingleton<IExpenseRepository>(ExpenseRepository(getIt()));
+  getIt.registerSingleton<IInsightRepository>(InsightRepository(getIt()));
+  getIt.registerSingleton<IUserPreferenceRepository>(
+      UserPreferenceRepository(getIt()));
 
-//   /// providers
-//   getIt.registerSingleton<SettingsProvider>(
-//     SettingsProvider(
-//       getUserPreferenceUsecase: getIt(),
-//       updateUserPreferenceUsecase: getIt(),
-//     )..getUserPref(),
-//   );
+  /// usecases
+  getIt.registerSingleton<GetExpensesUsecase>(GetExpensesUsecase(getIt()));
+  getIt.registerSingleton<CreateExpenseEntryUsecase>(
+      CreateExpenseEntryUsecase(getIt()));
+  getIt.registerSingleton<EraseEntriesUsecase>(EraseEntriesUsecase(getIt()));
+  getIt.registerSingleton<GetUserPreferenceUsecase>(
+      GetUserPreferenceUsecase(getIt()));
+  getIt.registerSingleton<UpdateUserPreferenceUsecase>(
+      UpdateUserPreferenceUsecase(getIt()));
+  getIt.registerSingleton<GetInsightsUsecase>(GetInsightsUsecase(getIt()));
 
-//   getIt.registerSingleton<ExpenseProvider>(
-//     ExpenseProvider(
-//       getExpensesUsecase: getIt(),
-//       createExpenseEntryUsecase: getIt(),
-//       eraseEntriesUsecase: getIt(),
-//     ),
-//   );
-//   getIt.registerSingleton<InsightsProvider>(
-//     InsightsProvider(
-//       getInsightsUsecase: getIt(),
-//     ),
-//   );
-// }
+  /// providers
+  getIt.registerSingleton<SettingsProvider>(
+    SettingsProvider(
+      getUserPreferenceUsecase: getIt(),
+      updateUserPreferenceUsecase: getIt(),
+    )..getUserPref(),
+  );
+
+  getIt.registerSingleton<ExpenseProvider>(
+    ExpenseProvider(
+      getExpensesUsecase: getIt(),
+      createExpenseEntryUsecase: getIt(),
+      eraseEntriesUsecase: getIt(),
+    ),
+  );
+  getIt.registerSingleton<InsightsProvider>(
+    InsightsProvider(
+      getInsightsUsecase: getIt(),
+    ),
+  );
+}
--- lib/features/main.dart
+++ lib/features/main.dart
@@ -1,42 +1,43 @@
-// import 'package:expense_bud/config/constants.dart';
-// import 'package:expense_bud/config/theme.dart';
-// import 'package:expense_bud/core/presentation/app.dart';
-// import 'package:expense_bud/core/presentation/onboarding.dart';
-// import 'package:expense_bud/features/expense/presentation/provider/expense_provider.dart';
-// import 'package:expense_bud/features/insights/presentation/provider/insights_provider.dart';
-// import 'package:expense_bud/features/settings/presentation/providers/settings_provider.dart';
-// import 'package:expense_bud/injector.dart';
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await initApp();
-//   runApp(const ExpenseTracker());
-// }
+import '../config/constant.dart';
+import '../config/theme.dart';
+import '../core/presentation/app.dart';
+import '../core/presentation/onboarding.dart';
+import 'expence/presentation/provider/expense_provider.dart';
+import 'injector.dart';
+import 'insights/presentation/provider/insights_provider.dart';
+import 'settings/presentation/providers/settings_provider.dart';
 
-// class ExpenseTracker extends StatelessWidget {
-//   const ExpenseTracker({Key? key}) : super(key: key);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initApp();
+  runApp(const ExpenseTracker());
+}
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MultiProvider(
-//       providers: [
-//         ChangeNotifierProvider<ExpenseProvider>(create: (context) => getIt()),
-//         ChangeNotifierProvider<InsightsProvider>(create: (context) => getIt()),
-//         ChangeNotifierProvider<SettingsProvider>(create: (context) => getIt()),
-//       ],
-//       child: Builder(builder: (context) {
-//         final onboardingComplete = context.select<SettingsProvider, bool>(
-//           (settings) => settings.preference.onboardingComplete,
-//         );
-//         return MaterialApp(
-//           debugShowCheckedModeBanner: false,
-//           theme: AppTheme.defaultTheme,
-//           title: AppStrings.kTitle,
-//           home: onboardingComplete ? const AppPage() : const OnboardingPage(),
-//         );
-//       }),
-//     );
-//   }
-// }
+class ExpenseTracker extends StatelessWidget {
+  const ExpenseTracker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ExpenseProvider>(create: (context) => getIt()),
+        ChangeNotifierProvider<InsightsProvider>(create: (context) => getIt()),
+        ChangeNotifierProvider<SettingsProvider>(create: (context) => getIt()),
+      ],
+      child: Builder(builder: (context) {
+        final onboardingComplete = context.select<SettingsProvider, bool>(
+          (settings) => settings.preference.onboardingComplete,
+        );
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.defaultTheme,
+          title: AppStrings.kTitle,
+          home: onboardingComplete ? const AppPage() : const OnboardingPage(),
+        );
+      }),
+    );
+  }
+}
